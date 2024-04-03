3537ca444aa2edf480165282b76268fd66da2376
Caio Ramos <aramoscaio@gmail.com>
2024-04-02 21:51:30 -0300

    feat: added endpoints to add and get information about employees

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, request
+import requests
+from functools import wraps
+
+app = Flask(__name__)
+
+employees = []
+
+def basic_auth(username, password):
+    return username == 'caio_ramos' and password == 'jose123'
+
+
+def auth_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not basic_auth(auth.username, auth.password):
+            return jsonify({'message': 'Authentication Failed. Please provide a valid username and password'}), 401
+        return f(*args, **kwargs)
+    return decorated
+
+@app.route('/api/employees', methods=['POST'])
+@auth_required
+def add_employee():
+    employee_data = request.get_json()
+    employees.append(employee_data)
+    return jsonify({'message': 'Successfully added employee'}), 201
+
+@app.route('/api/employees', methods=['GET'])
+def get_employees():
+    return jsonify(employees)
+
+if __name__ == '__main__':
+    app.run(debug=True)
